fd0d897788a079a79e38f11a50c237beb105552a
moralesl5 <loganjmorales@gmail.com>
2017-04-26 18:04:28 -0400

    Styled Pages
    Added CSS to pages for aesthetics, minor changes to source code to
    match new CSS

--- src/listQuestions.php
+++ src/listQuestions.php
@@ -3,17 +3,27 @@
 ?>
 
 <html>
-<header>
+<head>
   <title>Questions - IdeaWeb</title>
-</header>
+  <link href="css/master.css" rel="stylesheet">
+</head>
 
 <body>
+	<header>
+		<a href="home.html" class="header">Idea Web</a>
+	</header>
+ 
+ <div class="questionlist">
+ 
+ 	<p class="questionheader">Current questions:</p>
+ 	<br/>
   <?php 
   $result = getAllQuestions();
     while($row = $result->fetch_assoc()) {
-      echo '<div><a href="./question.php?id=' . $row['Id'] .'">' . $row['Title'] . '</a></div>';
+      echo '<div><a href="./question.php?id=' . $row['Id'] .'" class="questionlist">' . $row['Title'] . '</a></div>';
     }
   ?>
+  </div>
 </body>
 
 </html>
